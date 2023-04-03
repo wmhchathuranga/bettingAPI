30c3cf9a1473637ea35a0e9998e328cecb40270f
Harshana Chathuranga <91200650+wmhchathuranga@users.noreply.github.com>
2023-04-03 06:55:14 +0530

    no-sandbox added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ wss.on('connection', (ws) => {
 
     // Launch a new Puppeteer instance for this user
     (async () => {
-        const browser = await puppeteer.launch({ headless: true });
+        const browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
         const page = await browser.newPage();
 
         ws.on('message', async (data) => {
